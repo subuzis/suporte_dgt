d7a8b3e9d0cf34a8ad5933d95d64cf1095ec95a6
subuzis <lobat.dev@gmail.com>
2022-01-25 12:59:18 -0300

    carga inicial

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 from decouple import config, Csv
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('%_96gy-$*!7*^hi$tc8&_!2-9sxrb48p%x4wq7cb2qc84n#na_)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
